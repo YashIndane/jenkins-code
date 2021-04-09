0276a2c26e2956e7b1848b4e7210bd3998eb4f0b
Yash Indane <yashindane46@gmail.com>
2021-04-09 23:46:37 +0530

    Create basic_pipeline.groovy

---
+++ basic_pipeline.groovy
@@ -0,0 +1,30 @@
+pipeline{
+    
+    agent any
+    
+    stages{
+        
+        stage('Build'){
+            steps{
+               echo "hey we are building ..."
+               sleep 2
+            }   
+        }
+        
+        stage('Test'){
+            steps{
+               echo "mic ... check testing!"
+               sleep 2
+            }   
+        }
+        
+        stage('Deploy'){
+            steps{
+               echo "deploying... have patience"
+               sleep 2
+            }   
+        }
+        
+    }
+    
+}
